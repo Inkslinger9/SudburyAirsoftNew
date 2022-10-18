6b0f91a5e7724f4e5b94815ddbc5c7fd31d4a73b
Inkslinger9 <96968798+Inkslinger9@users.noreply.github.com>
2022-10-18 11:41:07 -0400

    added starting project

---
+++ script.js
@@ -0,0 +1,20 @@
+// var imageToEnlarge = document.getElementsByClassName("box") ;
+
+// var specifyImage = 0;
+
+// for (var i = 0; i< imageToEnlarge.length; i++){
+
+//     imageToEnlarge[i].addEventListener("click", changeButton);
+    
+    
+    
+    
+// }
+ 
+// function changeButton(){
+
+   
+//     imageToEnlarge[specifyImage].classList.toggle("enlarge-image");
+// }
+
+
